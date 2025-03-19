042aa067316fc089bdf67ba3e7f49a8764f5ad78
Emi Matsumura <emyu@Emis-air.local>
2025-03-19 16:01:02 +0900

    Remove venv directory from git tracking and update .gitignore

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import ifcopenshell
-file = ifcopenshell.open('/Users/emyu/Downloads/20250316.ifc')
+file = ifcopenshell.open('20241102.ifc')
 columns = file.by_type('IfcColumn')
 for i in columns:
     print(i.Name)
